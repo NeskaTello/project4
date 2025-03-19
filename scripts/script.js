67c0f0070c3e6cf105705b60784637356b9c6e57
NeskaTello <202809112+NeskaTello@users.noreply.github.com>
2025-03-18 21:05:12 -0400

    added the javascript

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Veroneska Tello
+    File Name: script.js
+    Date: 03/11/2024
+*/
+
+//JQuery for hero image to consume the header window space
+$(document).ready(function () {
+  $(".hero").height($(window).height());
+});
